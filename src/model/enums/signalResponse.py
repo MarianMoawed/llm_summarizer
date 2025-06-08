0b21ef79c9f23ed6e0c0528a002cc3f4a043f682
MarianMoawed <marianmoawed9@gmail.com>
2025-06-08 18:47:14 +0200

    added chunk and files signals

--- src/model/enums/signalResponse.py
+++ src/model/enums/signalResponse.py
@@ -6,3 +6,7 @@ class SignalResponse(Enum):
     FILE_TYPE_NOT_SUPPORTED_ERROR = "file_type_not_supported"
     FILE_UPLOADED_SUCCESSFULLY = "file_uploaded_successfully"
     FILE_SAVING_ERROR = "error_saving_file"
+    FILE_ID_ERROR = "no_file_found_with_this_id"
+    NO_ASSETS_FOUND_ERROR = "no_assets_found_for_this_project"
+    CHUNKS_DELETED_SUCCESSFULLY = "chunks_deleted_successfully"
+    CHUNKS_INSERTED_SUCCESSFULLY = "chunks_inserted_successfully"
