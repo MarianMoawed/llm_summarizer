c9fbe404aff0087be844ad573c9d8532c12c9b72
MarianMoawed <marianmoawed9@gmail.com>
2025-06-08 18:49:02 +0200

    created summarize text and summarize file routes

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 from helpers.config import get_settings
 from stores.llm.LLMEnums import LLMEnums
 from model.enums.signalResponse import SignalResponse
+from model.AssetModel import AssetModel
+from model.ChunkModel import ChunkModel
 
 settings=get_settings()
 
@@ -23,3 +25,22 @@ def summarize_text(text,request:Request):
         chat_history=LLMEnums.CHAT_HISTORY.value
         )
     return response
+
+
+@nlp_router.post("/summarize/{project_id}")
+async def summarize_file(request: Request,project_id:str, file_id: str):
+
+    asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
+    asset = await asset_model.get_asset_record(asset_project_id=project_id, asset_name=file_id)
+
+    chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
+    asset_chunks = await chunk_model.get_asset_chunks(project_id=project_id, asset_id=asset.id)
+
+    full_text = " ".join(chunk.chunk_content for chunk in asset_chunks)
+    summary =  request.app.generation_client.generate_text(
+        prompt=full_text,
+        max_tokens=settings.LLM_DEFAULT_MAX_TOKENS,
+        temperature= settings.LLM_DEFAULT_TEMPERATURE,
+        chat_history=LLMEnums.CHAT_HISTORY.value
+    )
+    return JSONResponse(content={"summary": summary})
